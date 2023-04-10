51a7683b980789c6694d0b1abb73f00c6977b26a
cemalokten <cemalokten@gmail.com>
2023-04-10 19:06:06 +0200

    Update with working GET

--- index.js
+++ index.js
@@ -1,85 +1,124 @@
-// Import Nodes built-in webserver module
 // const http = require('http')
-const express = require('express')
-const moment = require('moment')
-const morgan = require('morgan')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const express = require("express");
+const moment = require("moment");
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static('build'))
+dotenv.config();
 
-let persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
+app.use(cors());
+app.use(express.json());
+// app.use(express.static('build'))
+
+const password = process.env.MONGO_PASSWORD;
+const url = `mongodb+srv://admin:${password}@cluster0.dnn600n.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(url)
+  .then(() => console.log("Connected to the database"))
+  .catch((err) => console.log("Error connecting to the database:", err));
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+let persons;
+
+const getAllNames = async () => {
+  try {
+    const result = await Person.find({});
+    return result;
+  } catch (e) {
+    console.log(e);
   }
-];
+};
 
-morgan.token('id', function getId(req) {
-  const { name, number } = req.body
-  return JSON.stringify({ name, number })
-})
+app.get("/api/persons", async (req, res) => {
+  persons = await getAllNames();
+  res.json(persons);
+});
 
-app.use(morgan(':method :url :status :id - :response-time ms'))
+morgan.token("id", function getId(req) {
+  const { name, number } = req.body;
+  return JSON.stringify({ name, number });
+});
+
+app.use(morgan(":method :url :status :id - :response-time ms"));
 
 const info = `
-Phonebook has info for ${persons.length} people<br>
+Phonebook has info for ${persons?.length} people<br>
 <br>
-${moment().format('MMMM Do YYYY, h:mm:ss a')}
-`
+${moment().format("MMMM Do YYYY, h:mm:ss a")}
+`;
 
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
-})
+// Routes
 
-app.get('/info', (req, res) => {
-  res.send(info)
-})
+app.get("api/info", (req, res) => {
+  res.send(info);
+});
 
-app.get('/api/persons/:id', (req, res) => {
-  const { id } = req.params
-  const person = persons.filter((person) => person.id === Number(id))
-  if (person.length > 0) return res.json(person)
-  return res.status(404).end()
-})
+app.get("/api/persons/:id", (req, res) => {
+  const { id } = req.params;
+  const person = persons.filter((person) => person.id === Number(id));
+  if (person.length > 0) return res.json(person);
+  return res.status(404).end();
+});
 
-app.delete('/api/persons/delete/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter((p) => p.id !== id)
-  res.json().status(204).end()
-})
+app.delete("/api/persons/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    await Person.deleteOne({ _id: id });
+    res.status(204).end();
+  } catch (e) {
+    console.log(e);
+    res.status(500).send({ error: "Internal server error" });
+  }
+});
 
-app.post('/api/persons/', (req, res) => {
-  const { name, number } = req.body
-  if (name === undefined || number === undefined) return res.json({ error: 'The name or number is missing' }).status(404)
-  const checkName = persons.filter(({ name: _name }) => _name === name)
-  if (checkName.length > 0) return res.json({ error: 'name must be unique' }).status(404)
-  const id = Math.floor(Math.random() * 999)
-  persons.push({ id, name, number })
-  return res.json({ id, name, number }).status(204).end()
-})
+app.post("/api/persons/", async (req, res) => {
+  const { name, number } = req.body;
 
-const PORT = process.env.PORT || 3001
-  app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  if (name === undefined || number === undefined) {
+    return res.json({ error: "The name or number is missing" }).status(404);
+  }
+
+  const checkName = persons.filter(({ name: _name }) => _name === name);
+
+  if (checkName.length > 0)
+    return res.json({ error: "name must be unique" }).status(404);
+
+  try {
+    await Person.insertMany({ name, number });
+    res.status(204).end();
+  } catch (e) {
+    console.log(e);
+    res.status(500).send({ error: "Internal server error" });
+  }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// Close the database connection when the application terminates
+const closeConnectionAndExit = async () => {
+  try {
+    await mongoose.connection.close();
+    console.log("Database connection closed.");
+  } catch (err) {
+    console.error("Error closing the database connection:", err);
+  }
+  process.exit();
+};
+
+process.on("SIGINT", closeConnectionAndExit);
+process.on("SIGTERM", closeConnectionAndExit);
