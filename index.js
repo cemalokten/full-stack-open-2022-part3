6874945acecd143544793f56996290ff055393eb
cemalokten <cemalokten@gmail.com>
2023-04-11 21:11:13 +0200

    Add external functions and SIGTERM SIGINT disconnect()

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const express = require("express");
 const moment = require("moment");
 const morgan = require("morgan");
 const cors = require("cors");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const { getAllPersons, deleteOnePerson, findPersonById } = require("./mongo");
+const { connectDB, disconnectDB } = require("./connection");
 
 const app = express();
 
@@ -14,36 +15,12 @@ app.use(cors());
 app.use(express.json());
 // app.use(express.static('build'))
 
-const password = process.env.MONGO_PASSWORD;
-const url = `mongodb+srv://admin:${password}@cluster0.dnn600n.mongodb.net/?retryWrites=true&w=majority`;
-
-mongoose.set("strictQuery", false);
-mongoose
-  .connect(url)
-  .then(() => console.log("Connected to the database"))
-  .catch((err) => console.log("Error connecting to the database:", err));
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: Number,
-});
-
-const Person = mongoose.model("Person", personSchema);
 connectDB()
 
 let persons;
 
-const getAllNames = async () => {
-  try {
-    const result = await Person.find({});
-    return result;
-  } catch (e) {
-    console.log(e);
-  }
-};
-
 app.get("/api/persons", async (req, res) => {
-  persons = await getAllNames();
+  persons = await getAllPersons();
   res.json(persons);
 });
 
@@ -66,22 +43,14 @@ app.get("api/info", (req, res) => {
   res.send(info);
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", async (req, res) => {
   const { id } = req.params;
-  const person = persons.filter((person) => person.id === Number(id));
-  if (person.length > 0) return res.json(person);
-  return res.status(404).end();
+  await findPersonById(id, res)
 });
 
 app.delete("/api/persons/delete/:id", async (req, res) => {
   const id = req.params.id;
-  try {
-    await Person.deleteOne({ _id: id });
-    res.status(204).end();
-  } catch (e) {
-    console.log(e);
-    res.status(500).send({ error: "Internal server error" });
-  }
+  await deleteOnePerson({id, res})
 });
 
 app.post("/api/persons/", async (req, res) => {
@@ -110,16 +79,12 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-// Close the database connection when the application terminates
-const closeConnectionAndExit = async () => {
-  try {
-    await mongoose.connection.close();
-    console.log("Database connection closed.");
-  } catch (err) {
-    console.error("Error closing the database connection:", err);
-  }
-  process.exit();
-};
+process.on('SIGTERM', async () => {
+  console.log('SIGTERM disconnected from database')
+  await disconnectDB()
+})
 
-process.on("SIGINT", closeConnectionAndExit);
-process.on("SIGTERM", closeConnectionAndExit);
+process.on('SIGINT', async () => {
+  console.log('SIGINT disconnected from database')
+  await disconnectDB()
+})
