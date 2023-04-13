10439c064427c0a867069b8ce7899a57f01d2571
cemalokten <cemalokten@gmail.com>
2023-04-13 08:57:31 +0200

    Update express routes and error handling

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // const http = require('http')
 const express = require("express");
-const moment = require("moment");
 const morgan = require("morgan");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const { getAllPersons, deleteOnePerson, findPersonById } = require("./mongo");
+const {
+  getAllPersons,
+  addPerson,
+  deleteOnePerson,
+  findPersonById,
+  getInfo,
+} = require("./mongo");
 const { connectDB, disconnectDB } = require("./connection");
 
 const app = express();
@@ -15,14 +20,7 @@ app.use(cors());
 app.use(express.json());
 // app.use(express.static('build'))
 
-connectDB()
-
-let persons;
-
-app.get("/api/persons", async (req, res) => {
-  persons = await getAllPersons();
-  res.json(persons);
-});
+connectDB();
 
 morgan.token("id", function getId(req) {
   const { name, number } = req.body;
@@ -31,47 +29,34 @@ morgan.token("id", function getId(req) {
 
 app.use(morgan(":method :url :status :id - :response-time ms"));
 
-const info = `
-Phonebook has info for ${persons?.length} people<br>
-<br>
-${moment().format("MMMM Do YYYY, h:mm:ss a")}
-`;
-
-// Routes
-
-app.get("api/info", (req, res) => {
-  res.send(info);
+app.get("/api/persons", async (req, res, next) => {
+  const persons = await getAllPersons(next);
+  res.json(persons);
 });
 
-app.get("/api/persons/:id", async (req, res) => {
-  const { id } = req.params;
-  await findPersonById(id, res)
+app.get("/api/info", async (req, res, next) => {
+  await getInfo(res, next);
 });
 
-app.delete("/api/persons/delete/:id", async (req, res) => {
-  const id = req.params.id;
-  await deleteOnePerson({id, res})
+app.get("/api/persons/:id", async (req, res, next) => {
+  await findPersonById(req.params.id, res, next);
 });
 
-app.post("/api/persons/", async (req, res) => {
+app.delete("/api/persons/delete/:id", async (req, res, next) => {
+  await deleteOnePerson({ id: req.params.id }, res, next);
+});
+
+app.post("/api/persons/", async (req, res, next) => {
   const { name, number } = req.body;
+  await addPerson(name, number, res, next);
+});
 
-  if (name === undefined || number === undefined) {
-    return res.json({ error: "The name or number is missing" }).status(404);
-  }
-
-  const checkName = persons.filter(({ name: _name }) => _name === name);
-
-  if (checkName.length > 0)
-    return res.json({ error: "name must be unique" }).status(404);
-
-  try {
-    await Person.insertMany({ name, number });
-    res.status(204).end();
-  } catch (e) {
-    console.log(e);
-    res.status(500).send({ error: "Internal server error" });
-  }
+app.use((err, req, res, next) => {
+  console.log(err);
+  res
+    .status(err.status || 500)
+    .json({ error: err.message })
+    .end();
 });
 
 const PORT = process.env.PORT || 3001;
@@ -79,12 +64,13 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-process.on('SIGTERM', async () => {
-  console.log('SIGTERM disconnected from database')
-  await disconnectDB()
-})
+process.on("SIGTERM", async () => {
+  console.log("SIGTERM disconnected from database");
+  await disconnectDB();
+});
 
-process.on('SIGINT', async () => {
-  console.log('SIGINT disconnected from database')
-  await disconnectDB()
-})
+process.on("SIGINT", async () => {
+  console.log("SIGINT disconnected from database");
+  await disconnectDB();
+});
+
