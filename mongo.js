7ce41f2959e3a4106e14017e1d22515fef539e3c
cemalokten <cemalokten@gmail.com>
2023-04-15 23:51:56 +0200

    Update mongo functions error handling

--- mongo.js
+++ mongo.js
@@ -2,35 +2,45 @@ const mongoose = require("mongoose");
 const moment = require("moment");
 
 const personSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, minLength: 3, required: true },
   number: Number,
 });
 
 const Person = mongoose.model("Person", personSchema);
 
-const getAllPersons = async (next) => {
+const getAllPersons = async (next, res) => {
   try {
     const result = await Person.find({});
     return result;
   } catch (e) {
-    console.log(e);
+    console.log('getAllPersons', e);
     next({ status: 500, message: "Error: getAllPersons failed" });
   }
 };
 
 const addPerson = async (name, number, res, next) => {
   if (name === undefined || number === undefined) {
-    return res.next({ status: 404, message: "The name or number is missing" })
+    return next({ status: 400, message: "The name or number is missing" });
   }
   const persons = await Person.find({});
   const checkName = persons.filter(({ name: _name }) => _name === name);
   if (checkName.length > 0)
-    return next({ status: 404, message: "Name must be unique" });
+    return next({ status: 409, message: "Name must be unique" });
   try {
     await Person.insertMany({ name, number });
+    res.status(201).end();
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updatePerson = async (person, id, res, next) => {
+  try {
+    const { number } = person;
+    await Person.findByIdAndUpdate(id, { number }, { new: true });
     res.status(204).end();
   } catch (e) {
-    console.log(e);
+    console.log('Update person', e);
     next({ status: 500, message: "Internal server error" });
   }
 };
@@ -40,7 +50,7 @@ const deleteOnePerson = async ({ id }, res, next) => {
     await Person.deleteOne({ _id: id });
     res.status(204).end();
   } catch (e) {
-    console.log(e);
+    console.log('DeleteOnePerson', e);
     next({ status: 500, message: "Internal server error" });
   }
 };
@@ -51,7 +61,7 @@ const findPersonById = async (id, res, next) => {
     if (person) return res.json(person);
     next({ status: 404, message: "Person not found" });
   } catch (e) {
-    console.log(e);
+    console.log('findPersonById', e);
     next({ status: 500, message: "Internal server error" });
   }
 };
@@ -65,16 +75,26 @@ const getInfo = async (res, next) => {
   ${moment().format("MMMM Do YYYY, h:mm:ss a")}
   `;
     res.send(info);
-  } catch (e) {
-    console.log(e);
-    next({ status: 500, message: "Internal server error" });
+  } catch (error) {
+    next(error);
   }
+};
+
+const errorHandler = (error, request, response, next) => {
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'Malformatted ID' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  }
+  next(error);
 };
 
 module.exports = {
   getAllPersons,
   addPerson,
+  updatePerson,
   deleteOnePerson,
   findPersonById,
   getInfo,
+  errorHandler,
 };
