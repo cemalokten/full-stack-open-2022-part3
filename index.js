24fc6252acc9fa701a62b0939dcf701595029418
cemalokten <cemalokten@gmail.com>
2023-04-16 17:46:09 +0200

    Add eslint and fix errors

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // const http = require('http')
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const dotenv = require("dotenv");
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const dotenv = require('dotenv');
 const {
   getAllPersons,
   addPerson,
@@ -11,8 +11,8 @@ const {
   findPersonById,
   getInfo,
   errorHandler,
-} = require("./mongo");
-const { connectDB, disconnectDB } = require("./connection");
+} = require('./mongo');
+const { connectDB, disconnectDB } = require('./connection');
 
 const app = express();
 
@@ -20,57 +20,60 @@ dotenv.config();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('build'))
+app.use(express.static('build'));
 
 connectDB();
 
-morgan.token("id", function getId(req) {
+morgan.token('id', (req) => {
   const { name, number } = req.body;
   return JSON.stringify({ name, number });
 });
 
-app.use(morgan(":method :url :status :id - :response-time ms"));
+app.use(morgan(':method :url :status :id - :response-time ms'));
 
-app.get("/api/persons", async (req, res, next) => {
+app.get('/api/persons', async (req, res, next) => {
   const persons = await getAllPersons(next, res);
   res.json(persons);
 });
 
-app.get("/api/info", async (req, res, next) => {
+app.get('/api/info', async (req, res, next) => {
   await getInfo(res, next);
 });
 
-app.get("/api/persons/:id", async (req, res, next) => {
+app.get('/api/persons/:id', async (req, res, next) => {
   await findPersonById(req.params.id, res, next);
 });
 
-app.delete("/api/persons/delete/:id", async (req, res, next) => {
+app.delete('/api/persons/delete/:id', async (req, res, next) => {
   await deleteOnePerson({ id: req.params.id }, res, next);
 });
 
-app.post("/api/persons/", async (req, res, next) => {
+app.post('/api/persons/', async (req, res, next) => {
   const { name, number } = req.body;
   await addPerson(name, number, res, next);
 });
 
-app.put("/api/persons/update/:id", async (req, res, next) => {
+app.put('/api/persons/update/:id', async (req, res, next) => {
   const { id } = req.params;
   await updatePerson(req.body, id, res);
 });
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server running on port ${PORT}`);
 });
 
-process.on("SIGTERM", async () => {
-  console.log("SIGTERM disconnected from database");
+process.on('SIGTERM', async () => {
+  // eslint-disable-next-line no-console
+  console.log('SIGTERM disconnected from database');
   await disconnectDB();
 });
 
-process.on("SIGINT", async () => {
-  console.log("SIGINT disconnected from database");
+process.on('SIGINT', async () => {
+  // eslint-disable-next-line no-console
+  console.log('SIGINT disconnected from database');
   await disconnectDB();
 });
--- mongo.js
+++ mongo.js
@@ -1,12 +1,13 @@
-const mongoose = require("mongoose");
-const moment = require("moment");
+/* eslint-disable consistent-return */
+const mongoose = require('mongoose');
+const moment = require('moment');
 
 const personSchema = new mongoose.Schema({
-  name: { type: String, minLength: 3, required: [true, "Name is required"] },
+  name: { type: String, minLength: 3, required: [true, 'Name is required'] },
   number: {
     type: Number,
     minLength: 8,
-    required: [true, "Number is required"],
+    required: [true, 'Number is required'],
     validate: {
       validator(number) {
         return /^\d{2,3}-\d{7,8}$/.test(number);
@@ -16,25 +17,24 @@ const personSchema = new mongoose.Schema({
   },
 });
 
-const Person = mongoose.model("Person", personSchema);
+const Person = mongoose.model('Person', personSchema);
 
-const getAllPersons = async (next, res) => {
+const getAllPersons = async (next) => {
   try {
     const result = await Person.find({});
     return result;
   } catch (e) {
-    next({ status: 500, message: "Error: getAllPersons failed" });
+    next({ status: 500, message: 'Error: getAllPersons failed' });
   }
 };
 
 const addPerson = async (name, number, res, next) => {
   if (name === undefined || number === undefined) {
-    return next({ status: 400, message: "The name or number is missing" });
+    return next({ status: 400, message: 'The name or number is missing' });
   }
   const persons = await Person.find({});
   const checkName = persons.filter(({ name: _name }) => _name === name);
-  if (checkName.length > 0)
-    return next({ status: 409, message: "Name must be unique" });
+  if (checkName.length > 0) { return next({ status: 409, message: 'Name must be unique' }); }
   try {
     await Person.insertMany({ name, number });
     res.status(201).end();
@@ -49,7 +49,7 @@ const updatePerson = async (person, id, res, next) => {
     await Person.findByIdAndUpdate(id, { number }, { new: true });
     res.status(204).end();
   } catch (e) {
-    next({ status: 500, message: "Internal server error" });
+    next({ status: 500, message: 'Internal server error' });
   }
 };
 
@@ -58,7 +58,7 @@ const deleteOnePerson = async ({ id }, res, next) => {
     await Person.deleteOne({ _id: id });
     res.status(204).end();
   } catch (e) {
-    next({ status: 500, message: "Internal server error" });
+    next({ status: 500, message: 'Internal server error' });
   }
 };
 
@@ -66,9 +66,9 @@ const findPersonById = async (id, res, next) => {
   try {
     const person = await Person.findById(id);
     if (person) return res.json(person);
-    next({ status: 404, message: "Person not found" });
+    next({ status: 404, message: 'Person not found' });
   } catch (e) {
-    next({ status: 500, message: "Internal server error" });
+    next({ status: 500, message: 'Internal server error' });
   }
 };
 
@@ -78,7 +78,7 @@ const getInfo = async (res, next) => {
     const info = `
   Phonebook has info for ${totalPerson} people<br>
   <br>
-  ${moment().format("MMMM Do YYYY, h:mm:ss a")}
+  ${moment().format('MMMM Do YYYY, h:mm:ss a')}
   `;
     res.send(info);
   } catch (error) {
@@ -87,9 +87,9 @@ const getInfo = async (res, next) => {
 };
 
 const errorHandler = (error, request, response, next) => {
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "Malformatted ID" });
-  } else if (error.name === "ValidationError") {
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'Malformatted ID' });
+  } if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message });
   }
   next(error);
